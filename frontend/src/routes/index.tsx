b14b71302c802931b1840b110c425cafe3064df8
Michael Boulerice <michael.boulerice@gmail.com>
2026-06-09 15:42:20 +0000

    Added initial code for demoing BigCalendar

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { DemoCalendar } from '../components/calendar/Calendar'
+
 function Index() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-semibold">Common Fabric</h1>
+    <div className="flex flex-col min-h-screen items-center justify-center">
+      <h1 className="text-2xl font-semibold">Commons Fabric</h1>
+      <DemoCalendar />
     </div>
   )
 }
