4186d1ab40119e710018f2af5f651ac9a5e32aff
marcus-wrrn <marcuswrrn@gmail.com>
2026-07-07 21:47:38 -0400

    refactor: app factory

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,11 @@ import { requestLoggingMiddleware } from './middleware/logging';
 import path from 'path'
 import cors from 'cors'
 
+/**
+ * App factory for initializing middleware, setting CORS policy and establishing routes
+ * 
+ * @returns 
+ */
 export function createApp() {
   const app = express();
 
@@ -14,15 +19,17 @@ export function createApp() {
       ? true // allows any local device from any port
       : (process.env.CORS_ORIGIN || 'http://localhost:5173');
     app.use(cors({ origin: corsOrigin, credentials: true }));
-  }
-
-  // In production: serve the built Vite frontend
-  if (process.env.NODE_ENV === 'production') {
+  } else if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../frontend/dist')));
     app.get('/{*splat}', (_req, res) => {
       res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
     });
+  } else {
+    throw new Error("[SERVER] NODE_ENV not set, should be set to either: 'production' or 'development'");
   }
+
+  // route assignment
+
 
   return app;
 }
