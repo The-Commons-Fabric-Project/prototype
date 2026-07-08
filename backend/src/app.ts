7793b068e6b5c756b71bfa9e56fecb8b599fc325
marcus-wrrn <marcuswrrn@gmail.com>
2026-07-08 08:54:47 -0400

    added comments

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,6 +20,8 @@ export function createApp() {
       : (process.env.CORS_ORIGIN || 'http://localhost:5173');
     app.use(cors({ origin: corsOrigin, credentials: true }));
   } else if (process.env.NODE_ENV === 'production') {
+    // This code is for serving the built vite project, the backend router will override TanStack if they both have access to the same route 
+    // It serves a static html file if the path is not already registered on the backend
     app.use(express.static(path.join(__dirname, '../../frontend/dist')));
     app.get('/{*splat}', (_req, res) => {
       res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,10 +8,14 @@ async function main() {
     console.log(`[SERVER] Commons Fabric backend running on http://localhost:${PORT}`);
   })
 
+  // Shutdown handler logic - when running project in development mode tsx will spawn multiple child processes for watching/restarting the server
+  // These child processes are not shutdown properly during manual shutdown (for whatever reason) and are only killed after the parent processes is killed
+  // The shutdown handler prevents this by shutting down immediately after a SIGINT or SIGTERM is detected - not waiting for tsx
+  // Not needed in production since tsx will not be used but it can't hurt  
   const shutdown = () => {
     server.close(() => process.exit(0));
   };
-  
+
   process.on('SIGINT', shutdown);
   process.on('SIGTERM', shutdown);
 }
