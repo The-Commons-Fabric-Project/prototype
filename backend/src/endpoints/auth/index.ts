0c71382849345295225465dcb5c1e020cdc3e6e4
marcus-wrrn <marcuswrrn@gmail.com>
2026-08-02 19:31:59 -0400

    feat: added auth/profile route

--- backend/src/endpoints/auth/index.ts
+++ backend/src/endpoints/auth/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import createUser from './create-user.js'
 import login from './login.js'
 import logout from './logout.js'
+import profile from './profile.js'
 
 /**
  * Routes for auth-related paths (`/auth/...`). Mounted under /v1 in app.ts,
@@ -12,3 +13,4 @@ export const authRouter = Router()
 authRouter.use(createUser)
 authRouter.use(login)
 authRouter.use(logout)
+authRouter.use(profile)
