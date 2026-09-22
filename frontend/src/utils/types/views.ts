0f24f880ca8e5b89a2794fca386aabe17cf8e0bf
Shanel Wu <shanel.wu@colorado.edu>
2026-09-22 11:31:52 -0400

    refactor modals as a view type

--- frontend/src/utils/types/views.ts
+++ frontend/src/utils/types/views.ts
@@ -2,3 +2,17 @@ export type EventsView = "cards" | "calendar";
 export type CalendarView = "day" | "week" | "month";
 
 export const CALENDAR_MAX_PER_DAY = 3;
+
+/**
+ * mirror structure of variants.ts
+ * any new modals added, just add them here
+ */
+
+const Modals = {
+  0: "login", // LoginModal,
+  1: "create_account", // CreateAccountModal,
+  2: "event_detail", // EventDetailModal,
+  3: "create_event", // CreateEventModal,
+} as const;
+
+export type ModalOption = (typeof Modals)[keyof typeof Modals] | undefined;
